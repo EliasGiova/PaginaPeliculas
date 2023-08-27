e3c2a09500698cb3d698b5872737bc9fb317c38c
EliasGiova <eliasgiooovanella@gmail.com>
2023-08-27 01:57:34 -0300

    modificando el App.js utilizando Router, routes y Route

--- src/App.js
+++ src/App.js
@@ -1,64 +1,21 @@
 import './App.css';
-import Pelicula from './Pelicula';
-import PageWrapper from './PageWrapper';
-import Paginacion from './Paginacion';
-import { useEffect, useState } from 'react';
+import Blog from './Blog';
+import ListadoPeliculas from './ListadoPeliculas';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 
 function App() {
 
-  const [paginaActual, setPaginaActual] = useState(1);
-  const [peliculas, setPeliculas] = useState([]);
-  const TOTAL_POR_PAGINA = 5;
-
- // Renderiza Toda la Pagina
-  useEffect(() => {
-    buscarPeliculas();
-  }, []);
-
-  const buscarPeliculas = async () => {
-    let url = 'https://cors-anywhere.herokuapp.com/https://lucasmoy.dev/data/react/peliculas.json';
-    let respuesta = await fetch(url, {
-      "method": 'GET',
-      "mode": 'cors',
-      "headers": {
-        "Accept": 'application/json',
-        "Content-Type": 'application/json',
-      }
-    });
-    let json = await respuesta.json();
-    setPeliculas(json);
-  }
-
-  const getTotalPaginas = () => {
-    let cantidadTotalDePeliculas = peliculas.length;
-    return Math.ceil(cantidadTotalDePeliculas / TOTAL_POR_PAGINA);
-  }
-
-  let peliculasPorPaginas = peliculas.slice((paginaActual - 1) * TOTAL_POR_PAGINA,
-    paginaActual * TOTAL_POR_PAGINA);
-
   return (
-    <PageWrapper>
-
-      {peliculasPorPaginas.map(pelicula =>
-        <Pelicula titulo={pelicula.titulo}
-          calificacion={pelicula.calificacion}
-          director={pelicula.director}
-          actores={pelicula.actores}
-          fecha={pelicula.fecha}
-          duracion={pelicula.duracion}
-          img={pelicula.img}>
-          {pelicula.descripcion}
-        </Pelicula>
-      )}
-
-      <Paginacion pagina={paginaActual} total={getTotalPaginas()} onChange={(pagina) => {
-        setPaginaActual(pagina)
-      }
-      }
-      />
-    </PageWrapper>
+    <Router>
+      <Routes>
+        <Route path="/" element={<ListadoPeliculas />} />
+        <Route path="/Blog" element={<Blog />} />
+      </Routes>
+    </Router>
   );
+
 }
 
 export default App;
---
+++ src/ListadoPeliculas.js
@@ -0,0 +1,64 @@
+import './App.css';
+import Pelicula from './Pelicula';
+import PageWrapper from './PageWrapper';
+import Paginacion from './Paginacion';
+import { useEffect, useState } from 'react';
+
+function ListadoPeliculas() {
+
+  const [paginaActual, setPaginaActual] = useState(1);
+  const [peliculas, setPeliculas] = useState([]);
+  const TOTAL_POR_PAGINA = 5;
+
+ // Renderiza Toda la Pagina
+  useEffect(() => {
+    buscarPeliculas();
+  }, []);
+
+  const buscarPeliculas = async () => {
+    let url = 'https://cors-anywhere.herokuapp.com/https://lucasmoy.dev/data/react/peliculas.json';
+    let respuesta = await fetch(url, {
+      "method": 'GET',
+      "mode": 'cors',
+      "headers": {
+        "Accept": 'application/json',
+        "Content-Type": 'application/json',
+      }
+    });
+    let json = await respuesta.json();
+    setPeliculas(json);
+  }
+
+  const getTotalPaginas = () => {
+    let cantidadTotalDePeliculas = peliculas.length;
+    return Math.ceil(cantidadTotalDePeliculas / TOTAL_POR_PAGINA);
+  }
+
+  let peliculasPorPaginas = peliculas.slice((paginaActual - 1) * TOTAL_POR_PAGINA,
+    paginaActual * TOTAL_POR_PAGINA);
+
+  return (
+    <PageWrapper>
+
+      {peliculasPorPaginas.map(pelicula =>
+        <Pelicula titulo={pelicula.titulo}
+          calificacion={pelicula.calificacion}
+          director={pelicula.director}
+          actores={pelicula.actores}
+          fecha={pelicula.fecha}
+          duracion={pelicula.duracion}
+          img={pelicula.img}>
+          {pelicula.descripcion}
+        </Pelicula>
+      )}
+
+      <Paginacion pagina={paginaActual} total={getTotalPaginas()} onChange={(pagina) => {
+        setPaginaActual(pagina)
+      }
+      }
+      />
+    </PageWrapper>
+  );
+}
+
+export default ListadoPeliculas;
